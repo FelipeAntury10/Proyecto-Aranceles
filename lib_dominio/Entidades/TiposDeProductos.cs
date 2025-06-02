55efd8ff155e8e077d90d5232a5b8c745045577a
FelipeAntury10 <felipeanturyxd10@gmail.com>
2025-06-02 17:20:45 -0500

    Lib_dominio

---
+++ lib_dominio/Entidades/TiposDeProductos.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace lib_dominio.Entidades
+{
+    public class TiposDeProductos
+    {
+        //3 Atributos
+        [Key] public int Id { get; set; } // :) int? != int ; null : 0 
+        public string? Nombre { get; set; }
+        public string? EntidadRegulatoria { get; set; }
+    }
+}
